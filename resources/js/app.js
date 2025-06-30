8562bee595ccd5aef54622b73b650b833014ab81
Tania Choque <taniachque@gmail.com>
2025-06-29 20:16:25 -0400

    Setup VueJS configuration.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import Vue from 'vue';
+import axios from 'axios';
+import { BootstrapVue } from 'bootstrap-vue';
+
+Vue.use(BootstrapVue);
+Vue.prototype.$http = axios;
+
+const app = new Vue({
+    el: '#app',
+});
